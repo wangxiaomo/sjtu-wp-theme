dbce1894c1616ca17ed3adf65d5b69f70dc3133a
Shuotian Cheng <stcheng1989@gmail.com>
2014-11-03 08:33:38 +0000

    update slider style

--- template-home.php
+++ template-home.php
@@ -17,9 +17,6 @@ if ( have_posts() ) {
 		if ( $slider_prefix=='default' ) {
 			$slider_prefix='';
 		}
-
-		//include the page header template
-		//locate_template( array( 'includes/page-header.php' ), true, true );
 	}
 } ?>
 
@@ -87,7 +84,7 @@ if ( have_posts() ) {
                     $Steps: 1                                       //[Optional] Steps to go for each navigation request, default value is 1
                 }
             };
-            var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+            var jssor_slider1 = new $JssorSlider$("slider-container", options);
 
             //responsive code begin
             //you can remove responsive code if you don't want the slider scales while window resizes
@@ -109,16 +106,11 @@ if ( have_posts() ) {
                 $(window).bind('resize', ScaleSlider);
             }
 
-
-            //if (navigator.userAgent.match(/(iPhone|iPod|iPad)/)) {
-            //    $(window).bind("orientationchange", ScaleSlider);
-            //}
-            //responsive code end
         });
     </script>
     <!-- Jssor Slider Begin -->
     <!-- You can move inline styles to css file or css block. -->
-    <div id="slider1_container" style="position: relative; top: 0px; left: 0px; width: 600px; height: 280px; overflow: hidden; ">
+    <div id="slider-container">
 
         <!-- Loading Screen -->
         <div u="loading" style="position: absolute; top: 0px; left: 0px;">
@@ -131,7 +123,7 @@ if ( have_posts() ) {
         </div>
 
         <!-- Slides Container -->
-        <div u="slides" style="cursor: move; position: absolute; left: 0px; top: 0px; width: 600px; height: 280px; overflow: hidden;">
+        <div id="slides" u="slides">
             <div>
                 <img u="image" src="<?php echo get_stylesheet_directory_uri(); ?>/images/slider/01.jpg" />
             </div>
@@ -140,92 +132,17 @@ if ( have_posts() ) {
             </div>
         </div>
 
-        <!-- Bullet Navigator Skin Begin -->
-        <style>
-            /* jssor slider bullet navigator skin 05 css */
-            /*
-            .jssorb05 div           (normal)
-            .jssorb05 div:hover     (normal mouseover)
-            .jssorb05 .av           (active)
-            .jssorb05 .av:hover     (active mouseover)
-            .jssorb05 .dn           (mousedown)
-            */
-            .jssorb05 div, .jssorb05 div:hover, .jssorb05 .av {
-                background: url(<?php echo get_stylesheet_directory_uri(); ?>/images/slider/b10.png) no-repeat;
-                overflow: hidden;
-                cursor: pointer;
-            }
-
-            .jssorb05 div {
-                background-position: -7px -7px;
-            }
-
-                .jssorb05 div:hover, .jssorb05 .av:hover {
-                    background-position: -37px -7px;
-                }
-
-            .jssorb05 .av {
-                background-position: -67px -7px;
-            }
-
-            .jssorb05 .dn, .jssorb05 .dn:hover {
-                background-position: -97px -7px;
-            }
-        </style>
         <!-- bullet navigator container -->
-        <div u="navigator" class="jssorb05" style="position: absolute; bottom: 16px; right: 6px;">
+        <div id="bullet" u="navigator" class="jssorb05">
             <!-- bullet navigator item prototype -->
             <div u="prototype" style="POSITION: absolute; WIDTH: 16px; HEIGHT: 16px;"></div>
         </div>
-        <!-- Bullet Navigator Skin End -->
-        <!-- Arrow Navigator Skin Begin -->
-        <style>
-            /* jssor slider arrow navigator skin 12 css */
-            /*
-            .jssora12l              (normal)
-            .jssora12r              (normal)
-            .jssora12l:hover        (normal mouseover)
-            .jssora12r:hover        (normal mouseover)
-            .jssora12ldn            (mousedown)
-            .jssora12rdn            (mousedown)
-            */
-            .jssora12l, .jssora12r, .jssora12ldn, .jssora12rdn {
-                position: absolute;
-                cursor: pointer;
-                display: block;
-                background: url(<?php echo get_stylesheet_directory_uri(); ?>/images/slider/a18.png) no-repeat;
-                overflow: hidden;
-            }
 
-            .jssora12l {
-                background-position: -16px -37px;
-            }
-
-            .jssora12r {
-                background-position: -75px -37px;
-            }
-
-            .jssora12l:hover {
-                background-position: -136px -37px;
-            }
-
-            .jssora12r:hover {
-                background-position: -195px -37px;
-            }
-
-            .jssora12ldn {
-                background-position: -256px -37px;
-            }
-
-            .jssora12rdn {
-                background-position: -315px -37px;
-            }
-        </style>
         <!-- Arrow Left -->
-        <span u="arrowleft" class="jssora12l" style="width: 30px; height: 46px; top: 123px; left: 0px;">
+        <span id="arrowleft" u="arrowleft" class="jssora12l">
         </span>
         <!-- Arrow Right -->
-        <span u="arrowright" class="jssora12r" style="width: 30px; height: 46px; top: 123px; right: 0px">
+        <span id="arrowright" u="arrowright" class="jssora12r">
         </span>
 
     </div>
@@ -240,9 +157,10 @@ if ( have_posts() ) {
 		<div class="columns-wrapper">
 			<div class="left-column">
 				<div class="box-title-bar">
-					<h3><a href="weibo.com">新闻动态</a></h3>
-					<a class="alignbottom alignright" href="">更多</a>
+					<h3><a href="weibo.com">新闻动态 | NEWS</a></h3>
+					<a class="alignbottom alignright" href="">更多|More</a>
 				</div>
+                <hr color="red">
 				<?php
 					global $more;
 					query_posts( array ('category_name' => 'news') );
